383d85d722af2f8f0f33546b2d2b4ec4e86d005a
TrungViet234 <tr.trungviet04@gmail.com>
2026-01-23 23:18:10 +0700

    update url

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,3 +1,3 @@
 // config/api.js
-const BASE_URL = "http://location-based-food-street-guide-production.up.railway.app";
+const BASE_URL = "https://location-based-food-street-guide-production.up.railway.app";
 // const BASE_URL = "http://127.0.0.1:5000"; // dùng khi dev local
