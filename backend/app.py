811fa05ba8533304ef2f1190f78a4ce600911b24
pt287 <phattran280704@gmail.com>
2026-01-24 00:20:33 +0700

    fix app.py

--- backend/app.py
+++ backend/app.py
@@ -33,7 +33,10 @@ app.config.update(
 )
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db.init_app(app)
+
 
 @app.route("/")
 def home():
