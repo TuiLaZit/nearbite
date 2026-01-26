49d429a6aeb7f6fa7e347be29bc6800c81a25983
TuiLaZit <tr.trungviet04@gmail.com>
2026-01-26 10:49:10 +0700

    cap nhat map

--- frontend/src/pages/LocationTracker.jsx
+++ frontend/src/pages/LocationTracker.jsx
@@ -329,8 +329,8 @@ function LocationTracker() {
           zoomControl={true}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
           />
           
           <MapUpdater center={userLocation} />
